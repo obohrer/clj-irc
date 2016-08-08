d60aee772c6c01eb12dbad1988e53b7d3fc423de
obohrer <olivier.bohrer@gmail.com>
2016-08-08 21:55:17 +0200

    Prepare next iteration

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clj-irc "0.0.2"
+(defproject clj-irc "0.0.3-SNAPSHOT"
   :description "clojure irc client based on pircbotx"
   :dependencies [[org.clojure/clojure   "1.8.0"]
                  [org.pircbotx/pircbotx "1.9"]])
